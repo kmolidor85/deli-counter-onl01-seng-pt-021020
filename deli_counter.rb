f9aac98da2420dec3b04488edf9c270a0e4ba973
kyle molidor <k_molidor@yahoo.com>
2020-02-20 05:22:54 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,8 +1,12 @@
 katz_deli = []
 
-take_a_number(katz_deli, name)
-  katz_deli.push(name)
-  return "Welcome #{name}. You are number #{katz_deli.length} in line."
-
-current_line  
-  
+def line(katz_deli)
+  line_positions = [];
+  if katz_deli.empty?
+    puts "The line is currently empty."
+  else
+    number = 1
+    katz_deli.each do |customer|
+      line_positions << "#{number}. #{customer}"
+      number += 1
+    end
